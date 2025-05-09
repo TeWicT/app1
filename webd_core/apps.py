edb20e9822ab7d6423a2d7f02b8601d836e1f456
Evgeny <tewict3@yandex.ru>
2025-05-09 13:57:18 +0300

    новый гитигнор

--- webd_core/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebdCoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'webd_core'
--- webd_core/parsing.py
+++
@@ -1,3 +0,0 @@
-def query(cfg, params):
-    # TODO: implement logic from webd.parsing/query-with-form and xml traversal
-    return []
